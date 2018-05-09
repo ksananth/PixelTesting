4e8af0c8b567d04bd850719c2436ff4139a9ee68
ananthks <anan.busyguy007@gmail.com>
2018-05-09 08:02:06 +0200

    added style

--- app/controller.js
+++ app/controller.js
@@ -1,9 +1,10 @@
-app.controller("screenController", function($scope, $http, $filter,$rootScope,$location) {
+app.controller("screenController", function($scope, $http, $filter,$rootScope,$location,toaster) {
 	console.log('root'+$rootScope.screens);
 	$scope.selectedScreenName = $location.search().screen;
 	$scope.screensObj = ($scope.screens[$scope.selectedBrand.toLowerCase()]);
 	$scope.selectedScreenImg = $filter('filter')($scope.screensObj, {'screenName':$scope.selectedScreenName});
 
+ toaster.pop("data.status", "", $scope.selectedScreenName+" selected", 2000, 'trustedHtml');
 	$scope.getUrl = function(label){
     	return label.replace('../', '');
   	}
